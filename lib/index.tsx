c6789cbf01a3e3207a358ea75e2a20211624cd61
Liz Sciortino <sciortino.l@gmail.com>
2021-01-07 14:53:24 -0500

    add tips to assessmenttype

--- lib/index.tsx
+++ lib/index.tsx
@@ -86,6 +86,7 @@ export interface UnserializedAssessmentData {
   short_description: string;
   suggested_duration_in_minutes: number;
   time_text: string;
+  tips: string[];
   type_description: string;
   why_take: string;
 }
